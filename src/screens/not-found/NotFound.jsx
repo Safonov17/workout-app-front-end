1f1926877686e33714914530919b1a1ea54f8b89
I.Safonov <safonovil17@yandex.ru>
2023-11-28 10:33:03 +0300

    Create new files for setting up react-router-dom

---
+++ src/screens/not-found/NotFound.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout/Layout'
+
+const NotFound = () => {
+	return (
+		<>
+			<Layout heading='Page Not Found' />
+			<div className='wrapper-inner-page'>404 page not found</div>
+		</>
+	)
+}
+
+export default NotFound
