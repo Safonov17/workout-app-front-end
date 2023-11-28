5487b1ba0f41218a1b599f79b61dfb0bb71f99b7
I.Safonov <safonovil17@yandex.ru>
2023-11-28 14:40:04 +0300

    Added a button, did an authorization check and added a title

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,7 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+
+import Button from '../../components/ui/button/Button'
+
+import { useAuth } from '../../hooks/useAuth'
+
 import Layout from '../../components/Layout/Layout'
 
+import styles from './Home.module.scss'
+
 function Home() {
-	return <Layout bgImage='images/home-bg-1-1.jpg'></Layout>
+	const { isAuth } = useAuth()
+	const navigate = useNavigate()
+
+	return (
+		<Layout bgImage='images/home-bg-1-1.jpg'>
+			<Button clickHandler={() => navigate(isAuth ? '/new-workout' : '/auth')}>
+				{isAuth ? 'New' : 'Sign in'}
+			</Button>
+			<h1 className={styles.heading}>exercises for the shoulders</h1>
+			{/* Counters */}
+		</Layout>
+	)
 }
 
 export default Home
