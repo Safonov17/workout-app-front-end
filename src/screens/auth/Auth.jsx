6abee00cbeb09219ac6fbd33aff4ee0ce7767be3
I.Safonov <safonovil17@yandex.ru>
2023-11-29 18:41:47 +0300

    Added Loader and buttons

--- src/screens/auth/Auth.jsx
+++ src/screens/auth/Auth.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
+import Loader from '../../components/ui/Loader'
 import Button from '../../components/ui/button/Button'
 import Field from '../../components/ui/field/Field'
 
 import Layout from '../../components/Layout/Layout'
 
+import styles from './Auth.module.scss'
+
+const isLoading = false
+const isLoadingAuth = false
+
 const Auth = () => {
+	const [type, setType] = useState('auth')
+
 	const {
 		register,
 		handleSubmit,
@@ -19,37 +28,40 @@ const Auth = () => {
 	}
 
 	return (
-		<div>
-			<Layout heading='Sign in' bgImage='images/auth-bg-1.jpg'>
-				<div className='wrapper-inner-page'>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<Field
-							error={errors?.email?.message}
-							name='email'
-							register={register}
-							options={{
-								required: 'Email is required'
-							}}
-							type='text'
-							placeholder='Enter email'
-						/>
+		<>
+			<Layout heading='Sign in' bgImage='images/auth-bg-1.jpg' />
+			<div className='wrapper-inner-page'>
+				{(isLoading || isLoadingAuth) && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
+						type='text'
+						placeholder='Enter email'
+					/>
 
-						<Field
-							error={errors?.password?.message}
-							name='password'
-							register={register}
-							options={{
-								required: 'Password is required'
-							}}
-							type='password'
-							placeholder='Enter password'
-						/>
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='password'
+						placeholder='Enter password'
+					/>
 
-						<Button>Let's go</Button>
-					</form>
-				</div>
-			</Layout>
-		</div>
+					<div className={styles.buttons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+					</div>
+				</form>
+			</div>
+		</>
 	)
 }
 
