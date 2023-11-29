24a8541e2c942fccaf0a0c366b0b5928b769191b
I.Safonov <safonovil17@yandex.ru>
2023-11-29 17:36:16 +0300

    Created a login form using react-hook-form

--- src/screens/auth/Auth.jsx
+++ src/screens/auth/Auth.jsx
@@ -1,6 +1,35 @@
+import { useForm } from 'react-hook-form'
+
+import Button from '../../components/ui/button/Button'
+
+import Layout from '../../components/Layout/Layout'
+
 const Auth = () => {
+	const { register, handleSubmit } = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		console.log(data)
+	}
+
 	return (
-		<div>Auth</div>
+		<div>
+			<Layout heading='Sign in' bgImage='images/auth-bg-1.jpg'>
+				<div className='wrapper-inner-page'>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<input
+							type='text'
+							placeholder='Enter email'
+							{...register('email', {
+								required: 'Email is required'
+							})}
+						/>
+						<Button>Let's go</Button>
+					</form>
+				</div>
+			</Layout>
+		</div>
 	)
 }
 
