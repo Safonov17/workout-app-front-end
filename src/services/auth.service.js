864821fac674b3902c67edbe88cf710f4c3b02f4
I.Safonov <safonovil17@yandex.ru>
2023-11-30 10:59:46 +0300

    Created a class with a request to the server and a cookie record

---
+++ src/services/auth.service.js
@@ -0,0 +1,18 @@
+import Cookies from 'js-cookie'
+
+import { $axios } from '../api'
+
+class AuthService {
+	async main(type) {
+		try {
+			const { data } = await $axios.post(`/users/${type}`)
+			if (data.token) Cookies.set('red', data.token)
+
+			return data
+		} catch (error) {
+			throw new Error(error)
+		}
+	}
+}
+
+export default new AuthService()
