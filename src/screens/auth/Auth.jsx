8f2369866b59d94f3800110ca29127c1c9440702
I.Safonov <safonovil17@yandex.ru>
2023-11-30 14:26:37 +0300

    I moved all the logic into a separate file

--- src/screens/auth/Auth.jsx
+++ src/screens/auth/Auth.jsx
@@ -1,46 +1,27 @@
 import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
 
 import Loader from '../../components/ui/Loader'
 import Button from '../../components/ui/button/Button'
 import Field from '../../components/ui/field/Field'
+
+import { useAuth } from '../../hooks/useAuth'
 
 import Layout from '../../components/Layout/Layout'
 
 import AuthService from '../../services/auth.service'
 
 import styles from './Auth.module.scss'
+import { useAuthPage } from './useAuthPage'
 
 // const isLoading = false
 // const isLoadingAuth = false
 
 const Auth = () => {
-	const [type, setType] = useState('login')
-
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset
-	} = useForm({
-		mode: 'onChange'
-	})
-
-	const { mutate, isLoading } = useMutation(
-		['auth'],
-		({ email, password }) => AuthService.main(email, password, type),
-		{
-			onSuccess: data => {
-				alert('success')
-				reset()
-			}
-		}
-	)
-
-	const onSubmit = data => {
-		mutate(data)
-	}
+	const { errors, handleSubmit, isLoading, onSubmit, register, setType } =
+		useAuthPage()
 
 	return (
 		<>
