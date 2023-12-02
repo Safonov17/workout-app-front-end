24b3563e376887956683630e3f50812762e2710e
I.Safonov <safonovil17@yandex.ru>
2023-12-02 12:45:42 +0300

    Unimportant changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 import { defineConfig } from 'vite'
-import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src')
+			}
+		]
 	}
 })
