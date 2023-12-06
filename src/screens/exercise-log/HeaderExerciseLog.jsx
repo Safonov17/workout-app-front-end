a9577e052fe63b7de287ff341a376963d66e99b0
I.Safonov <safonovil17@yandex.ru>
2023-12-06 17:14:45 +0300

    Create HeaderExerciseLog component

---
+++ src/screens/exercise-log/HeaderExerciseLog.jsx
@@ -0,0 +1,37 @@
+import cn from 'clsx'
+
+import stylesLayout from '@/components/Layout/Layout.module.scss'
+import Header from '@/components/Layout/header/Header'
+
+import styles from './ExerciseLog.module.scss'
+
+const HeaderExerciseLog = ({ isSuccess, exerciseLog }) => {
+	return (
+		<div
+			className={cn(stylesLayout.wrapper, stylesLayout.otherPage)}
+			style={{ backgroundImage: `url('/images/ex-bg-1.jpg')`, height: 300 }}
+		>
+			<Header
+				backLink={
+					isSuccess ? `/workout/${exerciseLog.workoutLog}` : '/workouts'
+				}
+			/>
+
+			{isSuccess && (
+				<div className={styles.heading}>
+					<img
+						src={
+							import.meta.env.VITE_SERVER_URL + exerciseLog.exercise.iconPath
+						}
+						alt=''
+						height='34'
+						draggable='false'
+					/>
+					<h1 className={stylesLayout.heading}>{exerciseLog.exercise.name}</h1>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default HeaderExerciseLog
