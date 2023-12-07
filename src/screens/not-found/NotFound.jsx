024f615fd0522e1500e8179cdfa9f1e4816e43dc
I.Safonov <safonovil17@yandex.ru>
2023-12-07 16:32:02 +0300

    Added a transition to the login page

--- src/screens/not-found/NotFound.jsx
+++ src/screens/not-found/NotFound.jsx
@@ -1,9 +1,24 @@
-import Layout from '../../components/Layout/Layout'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import Layout from '@/components/Layout/Layout'
 
 const NotFound = () => {
+	const { isAuth } = useAuth()
+
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!isAuth) {
+			navigate('/auth')
+		}
+	}, [])
+
 	return (
 		<>
-			<Layout heading='Page Not Found' />
+			<Layout heading='Page not found' />
 			<div className='wrapper-inner-page'>404 page not found</div>
 		</>
 	)
