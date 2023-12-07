27d3f7c8670e0742c55a16be689062f3c1d5331d
I.Safonov <safonovil17@yandex.ru>
2023-12-07 16:26:04 +0300

    Added arguments for TableRow

--- src/screens/exercise-log/ExerciseLog.jsx
+++ src/screens/exercise-log/ExerciseLog.jsx
@@ -3,14 +3,23 @@ import Alert from '@/components/ui/alert/Alert'
 
 import { useExerciseLog } from './hooks/useExerciseLog'
 
+import ExerciseError from './ExerciseError'
 import styles from './ExerciseLog.module.scss'
 import HeaderExerciseLog from './HeaderExerciseLog'
 import TableHeader from './table/TableHeader'
 import TableRow from './table/TableRow'
 
 const ExerciseLog = () => {
-	const { exerciseLog, isLoading, isSuccess } = useExerciseLog()
-	
+	const {
+		exerciseLog,
+		isLoading,
+		isSuccess,
+		error,
+		getState,
+		onChangeState,
+		toggleTime
+	} = useExerciseLog()
+
 	return (
 		<>
 			<HeaderExerciseLog exerciseLog={exerciseLog} isSuccess={isSuccess} />
@@ -18,13 +27,20 @@ const ExerciseLog = () => {
 				className='wrapper-inner-page'
 				style={{ paddingLeft: 0, paddingRight: 0 }}
 			>
+				<ExerciseError errors={[error]} />
 				{isLoading ? (
 					<Loader />
 				) : (
 					<div className={styles.wrapper}>
 						<TableHeader />
 						{exerciseLog?.times.map(item => (
-							<TableRow item={item} key={item.id} />
+							<TableRow
+								getState={getState}
+								onChangeState={onChangeState}
+								toggleTime={toggleTime}
+								item={item}
+								key={item.id}
+							/>
 						))}
 					</div>
 				)}
