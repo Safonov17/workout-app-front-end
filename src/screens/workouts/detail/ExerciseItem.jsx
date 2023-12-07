9fe804a1e8f44d1c7d23aaeca999e77e11cfc6ba
I.Safonov <safonovil17@yandex.ru>
2023-12-07 16:32:27 +0300

    Base settings

--- src/screens/workouts/detail/ExerciseItem.jsx
+++ src/screens/workouts/detail/ExerciseItem.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import styles from './Workout.module.scss'
 
 const ExerciseItem = ({ exerciseLog }) => {
-	const nav = useNavigate()
+	const navigation = useNavigate()
 
 	return (
 		<div
@@ -14,14 +14,14 @@ const ExerciseItem = ({ exerciseLog }) => {
 		>
 			<button
 				aria-label='Move to exercise'
-				onClick={() => nav(`/exercise/${exerciseLog.id}`)}
+				onClick={() => navigation(`/exercise/${exerciseLog.id}`)}
 			>
 				<span>{exerciseLog.exercise.name}</span>
 				<img
 					src={import.meta.env.VITE_SERVER_URL + exerciseLog.exercise.iconPath}
 					height='34'
 					alt=''
-					draggable='false'
+					draggable={false}
 				/>
 			</button>
 		</div>
